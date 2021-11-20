77b4d2796e3382ff43bbf7da513c1a8370df71b6
M4teusPrado <mateus.silvaprado@gmail.com>
2021-11-20 16:08:56 -0300

    Modelagem do esqueleto

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+
+   
+    <v-system-bar app> system bar   </v-system-bar>
+    <v-app-bar clipped-left app > app bar </v-app-bar>
+    <v-navigation-drawer clipped  app> v-navegation-drawer </v-navigation-drawer>
+    
+    <v-main>
+      <v-container fluid>
+        v-content
+      </v-container>
+    </v-main>
+
+    <v-footer app> 
+        footer
+    </v-footer>
+
+    <v-bottom-navigation app> 
+      v-bottom-navigation
+    </v-bottom-navigation>
+
+</v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+<style scoped>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
   color: #42b983;
 }
 </style>
+
