17f15677f4e8d60adb8bc8e33d3f1c4b85130420
M4teusPrado <mateus.silvaprado@gmail.com>
2021-11-21 21:51:56 -0300

    refatoração do codigo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <v-app>
-    <v-system-bar  height="20" style="display: flex; justify-content: center" app> system bar   </v-system-bar>
-    <v-app-bar clipped-left style="display: flex; justify-content: center" app> app bar </v-app-bar>
-    <v-navigation-drawer clipped   style="display: flex; justify-content: center" app> v-navegation-drawer </v-navigation-drawer>
-    <v-container fluid style="height: 100%; display: flex; align-items: center; justify-content: center" app > v-content</v-container>
-    <v-footer  style="display: flex; justify-content: center"  app> footer </v-footer>
-    <v-bottom-navigation style="display: flex; justify-content: center" app>  v-bottom-navigation </v-bottom-navigation>
+    <v-system-bar  height="20" class="base" app> system bar   </v-system-bar>
+    <v-app-bar clipped-left class="base" app> app bar </v-app-bar>
+    <v-navigation-drawer clipped permanent  style="padding: 18% 0% 0%;" class="base" app> v-navegation-drawer </v-navigation-drawer>
+    <v-container fluid style="height: 100%" class="base" app> v-content</v-container>
+    <v-footer class="base" app> footer </v-footer>
+    <v-bottom-navigation  class="base" app>  v-bottom-navigation </v-bottom-navigation>
 </v-app>
 </template>
+
+<style scoped>
+  .base {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
